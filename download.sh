4110c4ff2a7656f0185a849c9978d9c3287fa774
Alisa Kubota <alisa@AlisanoMacBook-Air.local>
2020-04-23 04:56:22 +0900

    This is minimum data-set for trial of Local DB

---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://cernbox.cern.ch/index.php/s/QLB0LXI5hRQA7y8/download -O localdb-dump.tar.gz
+wget https://cernbox.cern.ch/index.php/s/tQFDcLwerbcxQEn/download -O localdb.tar.gz
